53d1ccfde28324aa39214a50a6bd787e7b2e0ab2
Arne Tarara <arne@datafuse.de>
2025-09-09 19:59:36 +0200

    Added first draft of OLS model with sample data and helper tools

--- tools/energy-logger.sh
+++ tools/energy-logger.sh
@@ -5,9 +5,11 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# 1) Create a unique log file in /tmp (e.g. /tmp/energy‑abcd1234.log)
+# 1) Create a unique log file in /tmp (e.g. /tmp/energy‑abcd1234.log) if nothing supplied. Otherwise take user supplied dir which must also be writeable
 LOGFILE=$(mktemp /tmp/energy-XXXXXXXX.log)
+
 chmod 777 $LOGFILE
+
 echo "Logging to $LOGFILE"
 
 # 2) Counters and timers
@@ -20,7 +22,7 @@ while : ; do
     sudo cat /sys/kernel/debug/energy/all >> "$LOGFILE"
     echo '-------'                    >> "$LOGFILE"
 
-    sleep 0.1                         # ≈100 Hz sample rate
+    sleep 1                         # ≈100 Hz sample rate
     ((iterations++))
 
     # every 10 s print how many iterations have run so far
